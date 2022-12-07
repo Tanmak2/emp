341d6f962ce0eefa9a91b4945a3b043a906a868c
Tanmak2 <whdcks3131@naver.com>
2022-12-07 17:49:37 +0900

    feat : emp스프링

---
+++ src/main/java/com/dw/emp/service/EmpService.java
@@ -0,0 +1,5 @@
+package com.dw.emp.service;
+
+public class EmpService {
+
+}
---
+++ src/main/java/com/dw/emp/vo/EmpVO.java
@@ -0,0 +1,16 @@
+package com.dw.emp.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmpVO {
+	private int empno;
+	private String ename;
+	private String job;
+	private String hiredate;
+	private int sal;
+	private String dname;
+	private String loc;
+}
